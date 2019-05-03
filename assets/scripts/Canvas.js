92e4a510d7f5181efcb46bf0e3e5d8cc599bd643
genxium <genxium@hotmail.com>
2019-05-03 12:48:45 +0800

    Drafted the utility `findNearbyNonBarrierGridByBreathFirstSearch`.

--- assets/scripts/Canvas.js
+++ assets/scripts/Canvas.js
@@ -5,19 +5,19 @@ const describe = function(discretePt) {
 const heuristicallyEstimatePathLength = function(p1, p2) {
   const absDx = Math.abs(p1.x - p2.x);
   const absDy = Math.abs(p1.y - p2.y);
-  return absDx + absDy;
+  return Math.sqrt(absDx * absDx + absDy * absDy);
 };
 
-const euclideanDistance = function(p1, p2) {
+const hamiltonDistance = function(p1, p2) {
   const absDx = Math.abs(p1.x - p2.x);
   const absDy = Math.abs(p1.y - p2.y);
-  return Math.sqrt(absDx * absDx + absDy * absDy);
+  return absDx + absDy;
 };
 
 window.cachedKnownBarrierGridDict = {};
 window.refreshCachedKnownBarrierGridDict = function(mapNode, barrierColliders, thisPlayerCollider) {
   cachedKnownBarrierGridDict = {};
-  let tiledMapIns = mapNode.getComponent(cc.TiledMap); // This is a magic name.
+  const tiledMapIns = mapNode.getComponent(cc.TiledMap); // This is a magic name.
   const mapSizeDiscrete = tiledMapIns.getMapSize();
   const uniformDiscreteMargin = 0;
 
@@ -27,14 +27,14 @@ window.refreshCachedKnownBarrierGridDict = function(mapNode, barrierColliders, t
       toCollidePolygon.push(aComp.node.position.add(p));
     }
 
-    for (var discretePosXInMap = uniformDiscreteMargin; discretePosXInMap < mapSizeDiscrete.width - uniformDiscreteMargin; ++discretePosXInMap) {
-      for (var discretePosYInMap = uniformDiscreteMargin; discretePosYInMap < mapSizeDiscrete.height - uniformDiscreteMargin; ++discretePosYInMap) {
+    for (let discretePosXInMap = uniformDiscreteMargin; discretePosXInMap < mapSizeDiscrete.width - uniformDiscreteMargin; ++discretePosXInMap) {
+      for (let discretePosYInMap = uniformDiscreteMargin; discretePosYInMap < mapSizeDiscrete.height - uniformDiscreteMargin; ++discretePosYInMap) {
         if (null != cachedKnownBarrierGridDict[discretePosXInMap]
           && true == cachedKnownBarrierGridDict[discretePosXInMap][discretePosYInMap]) {
           continue;
         }
 
-        var centreOfAnchorTileInMapNode = tileCollisionManager._continuousFromCentreOfDiscreteTile(mapNode, tiledMapIns, null, discretePosXInMap, discretePosYInMap);
+        let centreOfAnchorTileInMapNode = tileCollisionManager._continuousFromCentreOfDiscreteTile(mapNode, tiledMapIns, null, discretePosXInMap, discretePosYInMap);
 
         let toCollideCircle = {
           position: centreOfAnchorTileInMapNode.add(
@@ -81,17 +81,18 @@ const NEIGHBOUR_DISCRETE_OFFSETS = [{
 }];
 
 window.findPathWithMapDiscretizingAStar = function(continuousSrcPtInMapNode, continuousDstPtInMapNode, eps, thisPlayerCollider, barrierColliders, controlledPlayerColliders, mapNode, maxExpanderTrialCount) {
+  const tiledMapIns = mapNode.getComponent(cc.TiledMap); // This is a magic name.
+  const mapSizeDiscrete = tiledMapIns.getMapSize();
   if (null == maxExpanderTrialCount) {
-    maxExpanderTrialCount = 10000;
+    maxExpanderTrialCount = ((mapSizeDiscrete.width*mapSizeDiscrete.height) << 1);
   }
-  let tiledMapIns = mapNode.getComponent(cc.TiledMap); // This is a magic name.
   // [Phase#0] Discretize the mapNode to grids (be it ORTHO or ISOMETRIC w.r.t. orientation of the tiledMapIns) and mark `cachedKnownBarrierGridDict`.
   if (!cachedKnownBarrierGridDict) {
     window.refreshCachedKnownBarrierGridDict(mapNode, barrierColliders, thisPlayerCollider);
   }
 
-  const discreteSrcPtInMapNode = tileCollisionManager._continuousToDiscrete(mapNode, tiledMapIns, null, continuousSrcPtInMapNode, cc.v2(0, 0));
-  const discreteDstPtInMapNode = tileCollisionManager._continuousToDiscrete(mapNode, tiledMapIns, null, continuousDstPtInMapNode, cc.v2(0, 0));
+  const discreteSrcPos = tileCollisionManager._continuousToDiscrete(mapNode, tiledMapIns, continuousSrcPtInMapNode, cc.v2(0, 0));
+  const discreteDstPos = tileCollisionManager._continuousToDiscrete(mapNode, tiledMapIns, continuousDstPtInMapNode, cc.v2(0, 0));
 
   let openSetFromSrc = new Set();
   let closedSet = new Set();
@@ -99,15 +100,15 @@ window.findPathWithMapDiscretizingAStar = function(continuousSrcPtInMapNode, con
   let hTotal = {}; // Heuristically estimated total distance for path "srcPt -> k (must pass) -> dstPt". 
 
   // Initialization.
-  openSetFromSrc.add(describe(discreteSrcPtInMapNode));
-  dFromSrc[describe(discreteSrcPtInMapNode)] = {
-    pos: discreteSrcPtInMapNode,
+  openSetFromSrc.add(describe(discreteSrcPos));
+  dFromSrc[describe(discreteSrcPos)] = {
+    pos: discreteSrcPos,
     value: 0.0,
     pre: null // Always NULL for `dFromSrc`.
   };
-  hTotal[describe(discreteSrcPtInMapNode)] = {
-    pos: discreteSrcPtInMapNode,
-    value: dFromSrc[describe(discreteSrcPtInMapNode)].value + heuristicallyEstimatePathLength(discreteDstPtInMapNode, discreteDstPtInMapNode),
+  hTotal[describe(discreteSrcPos)] = {
+    pos: discreteSrcPos,
+    value: dFromSrc[describe(discreteSrcPos)].value + heuristicallyEstimatePathLength(discreteDstPos, discreteDstPos),
     pre: null
   };
   let expanderTrialCount = 0;
@@ -115,12 +116,17 @@ window.findPathWithMapDiscretizingAStar = function(continuousSrcPtInMapNode, con
   // Main iteration body.  
   while (0 < openSetFromSrc.size) {
     if (expanderTrialCount > maxExpanderTrialCount) {
-      cc.log(`No path for (${discreteSrcPtInMapNode.x}, ${discreteSrcPtInMapNode.y}) => (${discreteDstPtInMapNode.x}, ${discreteDstPtInMapNode.y}), returning after maxExpanderTrialCount == ${maxExpanderTrialCount} reached.`);
+      cc.log(`No path for (${discreteSrcPos.x}, ${discreteSrcPos.y}) => (${discreteDstPos.x}, ${discreteDstPos.y}), returning after maxExpanderTrialCount == ${maxExpanderTrialCount} reached.`);
       return null;
     }
     // [Phase#1] 
     let expanderKey = null;
     let expander = null;
+    /*
+    * TODO
+    * 
+    * Make `openSetFromSrc` a heap scored by `hTotal[candidateKey]` and still be popping `candidateKey`.
+    */
     for (let candidateKey of openSetFromSrc) {
       let candidate = hTotal[candidateKey];
       if (null != expander && (expander.value <= candidate.value)) continue;
@@ -130,11 +136,11 @@ window.findPathWithMapDiscretizingAStar = function(continuousSrcPtInMapNode, con
 
     if (null == expanderKey) {
       // Not reachable in this discretization. 
-      cc.log(`No path for (${discreteSrcPtInMapNode.x}, ${discreteSrcPtInMapNode.y}) => (${discreteDstPtInMapNode.x}, ${discreteDstPtInMapNode.y}), returning early.`);
+      cc.log(`No path for (${discreteSrcPos.x}, ${discreteSrcPos.y}) => (${discreteDstPos.x}, ${discreteDstPos.y}), returning early.`);
       return null;
     }
 
-    if (expander.pos.x == discreteDstPtInMapNode.x && expander.pos.y == discreteDstPtInMapNode.y) {
+    if (expander.pos.x == discreteDstPos.x && expander.pos.y == discreteDstPos.y) {
       // Found a path in this discretization. 
       let pathToRet = [];
       while (null != expander) {
@@ -156,49 +162,163 @@ window.findPathWithMapDiscretizingAStar = function(continuousSrcPtInMapNode, con
     closedSet.add(expanderKey);
 
     // [Phase#4] Traverse the neighbours of `expanderKey` and update accordingly.
-    for (let neighbourOff of NEIGHBOUR_DISCRETE_OFFSETS) {
-      const discreteNeighbourPtInMapNode = {
-        x: expander.pos.x + neighbourOff.dx,
-        y: expander.pos.y + neighbourOff.dy,
+    for (let neighbourOffset of NEIGHBOUR_DISCRETE_OFFSETS) {
+      const discreteNeighbourPos = {
+        x: expander.pos.x + neighbourOffset.dx,
+        y: expander.pos.y + neighbourOffset.dy,
       };
-      const discreteNeighbourPtInMapNodeDesc = describe(discreteNeighbourPtInMapNode);
-
-      if (true == closedSet.has(discreteNeighbourPtInMapNodeDesc)) {
+      if (discreteNeighbourPos.x < 0
+          ||
+          discreteNeighbourPos.x >= mapSizeDiscrete.width
+          ||
+          discreteNeighbourPos.y < 0
+          ||
+          discreteNeighbourPos.y >= mapSizeDiscrete.height
+          ) {
         continue;
       }
-      if (cachedKnownBarrierGridDict[discreteNeighbourPtInMapNode.x] && true == cachedKnownBarrierGridDict[discreteNeighbourPtInMapNode.x][discreteNeighbourPtInMapNode.y]) {
-        // cc.log(`discreteNeighbourPtInMapNode == (${discreteNeighbourPtInMapNode.x}, ${discreteNeighbourPtInMapNode.y}) is a knownBarrierGrid.`);
+      const discreteNeighbourPosDesc = describe(discreteNeighbourPos);
+
+      if (true == closedSet.has(discreteNeighbourPosDesc)) {
+        continue;
+      }
+      if (cachedKnownBarrierGridDict[discreteNeighbourPos.x] && true == cachedKnownBarrierGridDict[discreteNeighbourPos.x][discreteNeighbourPos.y]) {
+        // cc.log(`discreteNeighbourPos == (${discreteNeighbourPos.x}, ${discreteNeighbourPos.y}) is a knownBarrierGrid.`);
         continue;
       }
 
-      if (!openSetFromSrc.has(discreteNeighbourPtInMapNodeDesc)) {
-        openSetFromSrc.add(discreteNeighbourPtInMapNodeDesc);
+      if (!openSetFromSrc.has(discreteNeighbourPosDesc)) {
+        openSetFromSrc.add(discreteNeighbourPosDesc);
       }
-      const proposedNeighbourDFromSrcValue = dFromSrc[expanderKey].value + euclideanDistance(expander.pos, discreteNeighbourPtInMapNode);
+      const proposedNeighbourDFromSrcValue = dFromSrc[expanderKey].value + hamiltonDistance(expander.pos, discreteNeighbourPos);
 
-      if (null == dFromSrc[discreteNeighbourPtInMapNodeDesc]) {
-        dFromSrc[discreteNeighbourPtInMapNodeDesc] = {
-          pos: discreteNeighbourPtInMapNode,
+      if (null == dFromSrc[discreteNeighbourPosDesc]) {
+        dFromSrc[discreteNeighbourPosDesc] = {
+          pos: discreteNeighbourPos,
           value: proposedNeighbourDFromSrcValue,
           pre: null
         };
-        hTotal[discreteNeighbourPtInMapNodeDesc] = {
-          pos: discreteNeighbourPtInMapNode,
-          value: proposedNeighbourDFromSrcValue + heuristicallyEstimatePathLength(discreteNeighbourPtInMapNode, discreteDstPtInMapNode),
+        hTotal[discreteNeighbourPosDesc] = {
+          pos: discreteNeighbourPos,
+          value: proposedNeighbourDFromSrcValue + heuristicallyEstimatePathLength(discreteNeighbourPos, discreteDstPos),
           pre: hTotal[expanderKey],
         };
       } else {
-        const origNeighbourDFromSrcValue = dFromSrc[discreteNeighbourPtInMapNodeDesc].val;
+        const origNeighbourDFromSrcValue = dFromSrc[discreteNeighbourPosDesc].val;
         if (origNeighbourDFromSrcValue <= proposedNeighbourDFromSrcValue) {
           continue;
         }
-        dFromSrc[discreteNeighbourPtInMapNodeDesc].value = proposedNeighbourDFromSrcValue;
-        hTotal[discreteNeighbourPtInMapNodeDesc].value = proposedNeighbourDFromSrcValue + heuristicallyEstimatePathLength(discreteNeighbourPtInMapNode, discreteDstPtInMapNode);
-        hTotal[discreteNeighbourPtInMapNodeDesc].pre = hTotal[expanderKey];
+        dFromSrc[discreteNeighbourPosDesc].value = proposedNeighbourDFromSrcValue;
+        hTotal[discreteNeighbourPosDesc].value = proposedNeighbourDFromSrcValue + heuristicallyEstimatePathLength(discreteNeighbourPos, discreteDstPos);
+        hTotal[discreteNeighbourPosDesc].pre = hTotal[expanderKey];
       }
     }
   }
 
+  return null;
+};
+
+window.findNearbyNonBarrierGridByBreathFirstSearch = function(mapNode, continuousSrcPtInMapNode /* cc.v2 */, minRequiredLayerDistance, maxExpanderTrialCount) {
+  const tiledMapIns = mapNode.getComponent(cc.TiledMap); // This is a magic name.
+  const mapSizeDiscrete = tiledMapIns.getMapSize();
+  if (null == maxExpanderTrialCount) {
+    maxExpanderTrialCount = ((mapSizeDiscrete.width*mapSizeDiscrete.height) << 1);
+  }
+ 
+  const discreteSrcPos = tileCollisionManager._continuousToDiscrete(mapNode, tiledMapIns, continuousSrcPtInMapNode, cc.v2(0, 0));
+
+  let openDictFromSrc = new Set();
+  let bfsQueue= []; 
+  let closedSet = new Set();
+
+  // Initialization.
+  const starter = {
+    pos: discreteSrcPos,
+    layerDistance: 0,
+    pre: null,
+  };
+  openDictFromSrc[describe(discreteSrcPos)] = starter;
+  bfsQueue.push(discreteSrcPos);
+  let expanderTrialCount = 0;
+
+  // Main iteration body.  
+  while (0 < bfsQueue.length) {
+    if (expanderTrialCount > maxExpanderTrialCount) {
+      cc.log(`No nearby non-barrier grid found for (${discreteSrcPos.x}, ${discreteSrcPos.y}), returning after maxExpanderTrialCount == ${maxExpanderTrialCount} reached.`);
+      return null;
+    }
+
+    const expanderPos = bfsQueue.shift();
+    if (null == expanderPos) {
+      cc.log(`No nearby non-barrier grid found for (${discreteSrcPos.x}, ${discreteSrcPos.y}), no feasible expanderPos.`);
+      return null;
+    }
+    const expanderKey = describe(expanderPos);
+    const expander = openDictFromSrc[expanderKey];
+
+    ++expanderTrialCount;
+
+    delete openDictFromSrc[expanderKey]; // [SYNTAX NOTE] The actually heap content is still held by `expander` in the current closure. -- YFLu.
+    closedSet.add(expanderKey);
+
+    for (let neighbourOffset of NEIGHBOUR_DISCRETE_OFFSETS) {
+      const discreteNeighbourPos = {
+        x: expander.pos.x + neighbourOffset.dx,
+        y: expander.pos.y + neighbourOffset.dy,
+      };
+      if (discreteNeighbourPos.x < 0
+          ||
+          discreteNeighbourPos.x >= mapSizeDiscrete.width
+          ||
+          discreteNeighbourPos.y < 0
+          ||
+          discreteNeighbourPos.y >= mapSizeDiscrete.height
+          ) {
+        continue;
+      }
+
+      const discreteNeighbourPosDesc = describe(discreteNeighbourPos);
+
+      if (true == closedSet.has(discreteNeighbourPosDesc)) {
+        continue;
+      }
+
+      let existingDiscreteNeighbourObj = openDictFromSrc[discreteNeighbourPosDesc];
+      if (null == existingDiscreteNeighbourObj) {
+        const discreteNeighbour = {
+          pos: discreteNeighbourPos,
+          layerDistance: (expander.layerDistance + 1),
+          pre: expander,
+        };
+        existingDiscreteNeighbourObj = discreteNeighbour;
+        openDictFromSrc[discreteNeighbourPosDesc] = discreteNeighbour;
+        bfsQueue.push(discreteNeighbourPos);
+      } else {
+        const proposedLayerDistanceForNeighbour = (expander.layerDistance + 1);
+        if (proposedLayerDistanceForNeighbour < existingDiscreteNeighbourObj) {
+          existingDiscreteNeighbourObj.layerDistance = proposedLayerDistanceForNeighbour;
+          existingDiscreteNeighbourObj.pre = expander;
+        }
+      }
+
+      const theEffectiveDiscreteNeighbourLayerDistance = existingDiscreteNeighbourObj.layerDistance;
+      if (
+          theEffectiveDiscreteNeighbourLayerDistance >= minRequiredLayerDistance
+          &&
+          (
+          null == cachedKnownBarrierGridDict[discreteNeighbourPos.x] 
+          || 
+          null == cachedKnownBarrierGridDict[discreteNeighbourPos.x][discreteNeighbourPos.y]
+          ||
+          false == cachedKnownBarrierGridDict[discreteNeighbourPos.x][discreteNeighbourPos.y]
+          )
+        ) {
+        // Found a feasible solution.
+        const continuousNeighbourPtInMapNode = tileCollisionManager._continuousFromCentreOfDiscreteTile(mapNode, tiledMapIns, null, discreteNeighbourPos.x, discreteNeighbourPos.y);
+        return continuousNeighbourPtInMapNode;  
+      }
+    }
+  }
   return null;
 };
 
